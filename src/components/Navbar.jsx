2f6dc40523e47b866f058497cf4a6f4fd6406b9d
adolpho <adolphovt12@gmail.com>
2023-12-03 19:55:39 -0600

    Ajuste de la barra

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import {Link} from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
-import { IoPerson, IoLogOut} from 'react-icons/io5'
+import { IoPerson, IoLogOut, IoChevronDownSharp, IoBagAdd, IoBagSharp} from 'react-icons/io5'
+import {Fragment} from 'react'
+import {Menu, Transition} from '@headlessui/react'
 
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+  
 
 function Navbar (){
     const {isAuthenticated, logout, user} = useAuth();
@@ -20,35 +26,176 @@ function Navbar (){
             {
                 isAuthenticated ? (
                     <>
-                    <Link to={
-            '/provedores'
-        }>
-        <h1 className="text-md font-bold">Proveedores</h1>
-        </Link>
-        <Link to={ '/clientes'
-        }>
-        <h1 className="text-md font-bold">Clientes</h1>
-        </Link>
                     <li className='text-lg px-5 '>
                         <div className='flex mx-3 px-3' >
                          <IoPerson size={30}/>{user.username}
                         </div>
                     </li>
                     <li>
-                        <Link to='/products/new'
-                        className='bg-slate-500 px-4 py-1 rounded-sm'
-                        >Agregar Producto</Link>
+                    <Menu as="div" className="relative inline-block text-left">
+                        <div>
+                          <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-zinc-700 px-3 py-2 text-sm 
+                                        ring-1 ring-inset ring-zinc-700 hover:bg-zinc-800">
+                          <IoBagSharp className="-mr-1 h-5 w-5"/> Productos
+                            <IoChevronDownSharp className="-mr-1 h-5 w-5" aria-hidden="true" />
+                          </Menu.Button>
+                        </div>
+
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right 
+                                    divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black 
+                                    ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                            <Menu.Item>
+                                {({ active }) => (
+                                  <Link
+                                    to="/products"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    <IoBagSharp className="h-5 w-5 inline-flex"/> Listar 
+                                  </Link>
+                                )}
+                              </Menu.Item>
+
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <Link
+                                    to="/products/new"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                  <IoBagAdd className="h-5 w-5 inline-flex"/>  Agregar 
+                                  </Link>
+                                )}
+                              </Menu.Item>
+                            </div>
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
                     </li>
                     <li>
-                        <Link to='/provedor/new'
-                        className='bg-slate-500 px-4 py-1 rounded-sm'
-                        >Agregar Proveedor</Link>
+                    <Menu as="div" className="relative inline-block text-left">
+                        <div>
+                          <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-zinc-700 px-3 py-2 text-sm 
+                                        ring-1 ring-inset ring-zinc-700 hover:bg-zinc-800">
+                          <IoBagSharp className="-mr-1 h-5 w-5"/> Proveedores
+                            <IoChevronDownSharp className="-mr-1 h-5 w-5" aria-hidden="true" />
+                          </Menu.Button>
+                        </div>
+
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right 
+                                    divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black 
+                                    ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                            <Menu.Item>
+                                {({ active }) => (
+                                  <Link
+                                    to="/provedores"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    <IoBagSharp className="h-5 w-5 inline-flex"/> Listar 
+                                  </Link>
+                                )}
+                              </Menu.Item>
+
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <Link
+                                    to="/provedor/new"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                  <IoBagAdd className="h-5 w-5 inline-flex"/>  Agregar 
+                                  </Link>
+                                )}
+                              </Menu.Item>
+                            </div>
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
                     </li>
                     <li>
-                        <Link to='/cliente/new' className='bg-slate-500 px-4 py-1 rounded-sm'
-                        >Agregar Cliente
-                        
-                        </Link>
+                    <Menu as="div" className="relative inline-block text-left">
+                        <div>
+                          <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-zinc-700 px-3 py-2 text-sm 
+                                        ring-1 ring-inset ring-zinc-700 hover:bg-zinc-800">
+                          <IoBagSharp className="-mr-1 h-5 w-5"/> Clientes
+                            <IoChevronDownSharp className="-mr-1 h-5 w-5" aria-hidden="true" />
+                          </Menu.Button>
+                        </div>
+
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right 
+                                    divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black 
+                                    ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                            <Menu.Item>
+                                {({ active }) => (
+                                  <Link
+                                    to="/clientes"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    <IoBagSharp className="h-5 w-5 inline-flex"/> Listar 
+                                  </Link>
+                                )}
+                              </Menu.Item>
+
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <Link
+                                    to="/cliente/new"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                  <IoBagAdd className="h-5 w-5 inline-flex"/>  Agregar 
+                                  </Link>
+                                )}
+                              </Menu.Item>
+                            </div>
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+                    
                     </li>
                     <li>
                         <Link to='/' onClick={()=>{logout()}}
